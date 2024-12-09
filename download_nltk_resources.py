8968dcd34561f5eca8f191e0ce1cb61db43875a9
mahlatselelosa <167199173+mahlatselelosa@users.noreply.github.com>
2024-12-09 20:02:26 +0200

    Add files via upload

---
+++ download_nltk_resources.py
@@ -0,0 +1,9 @@
+import nltk
+import os
+
+# Set custom NLTK data path
+#nltk.data.path.append(os.path.join(os.path.expanduser("~"), "nltk_data"))
+
+# Download required NLTK resources
+nltk.download()
+#nltk.download('stopwords')
